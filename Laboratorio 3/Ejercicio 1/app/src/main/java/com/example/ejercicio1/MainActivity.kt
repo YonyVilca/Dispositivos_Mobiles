781b4e8909bfa8e38d3e2830406f4eab59bd8cd3
YonyVilca <yvilcam@ulasalle.edu.pe>
2024-04-16 09:47:39 -0500

    Agregando comentarios y documentación, finalizando ejercicio

--- Laboratorio 3/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ Laboratorio 3/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -7,19 +7,19 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-
+// Clase principal de la actividad
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+// Obtener referencia al ImageButton en el layout
         val imageView = findViewById<ImageButton>(R.id.img_view)
 
-        // Agregar un listener al clic de la imagen
+// Configurar el listener para el ImageButton
         imageView.setOnClickListener {
-            // Mostrar un Toast con un mensaje personalizado
-            Toast.makeText(this, "¡Clic en la imagen!", Toast.LENGTH_SHORT).show()
+            // Mostrar un Toast con el mensaje "¡Universidad la Salle!"
+            Toast.makeText(this, "¡Universidad la Salle!", Toast.LENGTH_SHORT).show()
         }
     }
 }
