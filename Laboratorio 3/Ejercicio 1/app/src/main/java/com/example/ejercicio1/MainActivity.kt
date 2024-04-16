6e51348b0bb707fd6e824b7b7fe320fe56c65f13
YonyVilca <yvilcam@ulasalle.edu.pe>
2024-04-15 23:40:32 -0500

    Agregando la imagen y ajustando el tamaño con scaleType

--- Laboratorio 3/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ Laboratorio 3/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.ejercicio1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val imageView = findViewById<ImageButton>(R.id.img_view)
+
+        // Agregar un listener al clic de la imagen
+        imageView.setOnClickListener {
+            // Mostrar un Toast con un mensaje personalizado
+            Toast.makeText(this, "¡Clic en la imagen!", Toast.LENGTH_SHORT).show()
         }
     }
 }
