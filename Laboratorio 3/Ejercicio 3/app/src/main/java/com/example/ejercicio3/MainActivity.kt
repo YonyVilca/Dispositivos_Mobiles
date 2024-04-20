c369aef116806df9e12ba04522e7a5f04339261e
YonyVilca <yvilcam@ulasalle.edu.pe>
2024-04-19 22:50:23 -0500

    Agregando comentarios

--- Laboratorio 3/Ejercicio 3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
+++ Laboratorio 3/Ejercicio 3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
@@ -9,27 +9,31 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
+    // Declaramos una variable para almacenar el reproductor de medios
     private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+// Obtenemos referencias a los botones de la interfaz de usuario
         val buttonPlay: Button = findViewById(R.id.button)
         val buttonPause: Button = findViewById(R.id.button2)
         val buttonStop: Button = findViewById(R.id.button3)
-
+// Creamos el reproductor de medios y lo inicializamos con el archivo de audio en res/raw
         mediaPlayer = MediaPlayer.create(this, R.raw.music)
-
+// Configuramos el listener de clic para el botón de reproducir
         buttonPlay.setOnClickListener{
+            // Iniciamos la reproducción del audio
             mediaPlayer.start()
         }
+        // Configuramos el listener de clic para el botón de pausa
         buttonPause.setOnClickListener{
+            // Verificamos si el audio está reproduciéndose y lo pausamos si es así
             if (mediaPlayer.isPlaying) {
                 mediaPlayer.pause()
             }
         }
+        // Configuramos el listener de clic para el botón de detener
         buttonStop.setOnClickListener {
             mediaPlayer.stop()
             mediaPlayer.prepare()
@@ -37,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 }
-
+    // Detenemos la reproducción del audio, lo preparamos para reproducir desde el principio y lo llevamos al inicio
     override fun onDestroy() {
         super.onDestroy()
         mediaPlayer.release()
