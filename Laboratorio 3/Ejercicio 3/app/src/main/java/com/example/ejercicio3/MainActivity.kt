19a43122e3810ec3f2c12477a423420cacff727b
YonyVilca <yvilcam@ulasalle.edu.pe>
2024-04-19 22:39:57 -0500

    se incluyo el objeto media player y la logica del reproductor;

--- Laboratorio 3/Ejercicio 3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
+++ Laboratorio 3/Ejercicio 3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.ejercicio3
 
+import android.media.MediaPlayer
+import android.widget.Button
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +9,37 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonPlay: Button = findViewById(R.id.button)
+        val buttonPause: Button = findViewById(R.id.button2)
+        val buttonStop: Button = findViewById(R.id.button3)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
+
+        buttonPlay.setOnClickListener{
+            mediaPlayer.start()
         }
+        buttonPause.setOnClickListener{
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+            }
+        }
+        buttonStop.setOnClickListener {
+            mediaPlayer.stop()
+            mediaPlayer.prepare()
+            mediaPlayer.seekTo(0)
+
+        }
+}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 }
